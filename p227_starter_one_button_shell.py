c192a310d8b8f65366c6101fbeb5e019f43afb67
Malcolm <Malcolm.McNeill@crpusd.org>
2026-01-12 10:06:28 -0800

    modifies ping button

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -16,7 +16,8 @@ frame = tk.Frame(root)
 frame.pack()
 
 # set up button to run the do_command function
-ping_btn = tk.Button(frame, text="ping", command=do_command)
+# Makes the command button pass it's name to a function using lambda
+ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", command=lambda:do_command("ping"))
 ping_btn.pack()
 
 # creates the frame with label for the text box
