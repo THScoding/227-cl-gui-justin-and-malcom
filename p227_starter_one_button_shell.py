6571a43f445008f0c6ea26eecdd71738032c794b
Malcolm <Malcolm.McNeill@crpusd.org>
2026-01-12 10:01:02 -0800

    Add Url box and output box

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -19,4 +19,28 @@ frame.pack()
 ping_btn = tk.Button(frame, text="ping", command=do_command)
 ping_btn.pack()
 
+# creates the frame with label for the text box
+frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
+frame_URL.pack()
+
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+    compound="center",
+    font=("comic sans", 14),
+    bd=0, 
+    relief=tk.FLAT, 
+    cursor="heart",
+    fg="mediumpurple3",
+    bg="black")
+url_label.pack(side=tk.LEFT)
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry.pack(side=tk.LEFT)
+
+frame = tk.Frame(root,  bg="black") # change frame color
+frame.pack()
+
+# Adds an output box to GUI.
+command_textbox = tksc.ScrolledText(frame, height=10, width=100)
+command_textbox.pack()
+
 root.mainloop()
