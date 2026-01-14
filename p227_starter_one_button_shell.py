9389025cb6c74b97f303377c9d8465db8c8206cc
Malcolm <Malcolm.McNeill@crpusd.org>
2026-01-14 08:42:33 -0800

    save button, and working do_command function

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -4,12 +4,33 @@ import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
-def do_command():
-    command = ["ping", "tracert",'nslookup','netstat']
-    # Windows version to limit to 4 requests: command = ["ping", "localhost", "-n", "4"]
-    # Mac version to limit to 4 requests:     command = ["ping", "localhost", "-n", "4"]
+def do_command(command):
+    global command_textbox, url_entry
+
+    # If url_entry is blank, use localhost IP address 
+    url_val = url_entry.get()
+    if (len(url_val) == 0):
+        # url_val = "127.0.0.1"
+        url_val = "::1"
     
+    command_textbox.delete(1.0, tk.END)
+    command_textbox.insert(tk.END, command + " working....\n")
+    command_textbox.update()
+
+    with subprocess.Popen(command + ' ' + url_val, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            command_textbox.insert(tk.END,line)
+            command_textbox.update()
     subprocess.run(command)
+def mSave():
+  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+  if filename is None:
+    return
+  file = open (filename, mode = 'w')
+  text_to_save = command_textbox.get("1.0", tk.END)
+  
+  file.write(text_to_save)
+  file.close()
     
 root = tk.Tk()
 frame = tk.Frame(root)
@@ -28,6 +49,9 @@ nslookup_btn.pack()
 
 netstat_btn = tk.Button(frame, text="Network Status", command=lambda:do_command("netstat"))
 netstat_btn.pack()
+
+save_btn = tk.Button(frame, text="Save", command=lambda:mSave())
+save_btn.pack()
 
 # creates the frame with label for the text box
 frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
