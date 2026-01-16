ecddb734fb40ff9aa068e378a7f0df660c489b67
Malcolm <Malcolm.McNeill@crpusd.org>
2026-01-15 19:39:44 -0800

    fixed button updates

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -4,25 +4,27 @@ import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
-
 def do_command(command):
+   global selected_item
+   if selected_item == "ipconfig":
+     url_val = "/all"
+   else:
     global command_textbox, url_entry
-
     # If url_entry is blank, use localhost IP address 
     url_val = url_entry.get()
     if (len(url_val) == 0):
         # url_val = "127.0.0.1"
         url_val = "::1"
     
-    command_textbox.delete(1.0, tk.END)
-    command_textbox.insert(tk.END, command + " working....\n")
-    command_textbox.update()
+   command_textbox.delete(1.0, tk.END)
+   command_textbox.insert(tk.END, command + " working....\n")
+   command_textbox.update()
 
-    with subprocess.Popen(command + ' ' + url_val, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+   with subprocess.Popen(command + ' ' + url_val, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
         for line in p.stdout:
             command_textbox.insert(tk.END,line)
             command_textbox.update()
-    subprocess.run(command)
+   subprocess.run(command)
 def mSave():
   filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
   if filename is None:
@@ -31,7 +33,7 @@ def mSave():
   text_to_save = command_textbox.get("1.0", tk.END)
   
   file.write(text_to_save)
-  file.close()
+  file.close()  
 def get_selected_item():
     """Retrieves the currently selected item from the listbox and displays it in a message box."""
     # Get the index of the selected item
@@ -39,21 +41,22 @@ def get_selected_item():
     if selected_indices:
         # Get the actual value using the index (curselection returns a tuple of indices)
         index = selected_indices[0]
+        global oneRingBtn, selected_item
         selected_item = listbox.get(index)
+        oneRingBtn.destroy() 
         oneRingBtn = tk.Button(frame, text=selected_item, command=lambda:do_command(selected_item))
-        oneRingBtn.pack()
+        oneRingBtn.pack() 
         print(s.get())
-
-
+selected_item = "ping"
 root = tk.Tk()
 frame = tk.Frame(root)
+
 def run():
  global frame, var
  frame.pack()
  var = 1
  var +=1
  print(var)
-
 
 # set up button to run the do_command function
 # Makes the command button pass it's name to a function using lambd
@@ -73,10 +76,14 @@ save_btn.pack()
 
   # Adds an output box to GUI.
 command_textbox = tksc.ScrolledText(frame, height=10, width=100)
+
 command_textbox.pack()
 
 s = tk.Scale(frame, orient="horizontal", length=200, from_=1.0, to=100.0)
 s.pack()
+
+oneRingBtn = tk.Button(frame, text=selected_item, command=lambda:do_command(selected_item))
+oneRingBtn.pack() 
 
 root = tk.Tk()
 frame2 = tk.Frame(root)
@@ -101,7 +108,5 @@ url_label = tk.Label(frame2, text="Enter a URL of interest: ",
 url_label.pack(side=tk.LEFT)
 url_entry= tk.Entry(frame2,  font=("comic sans", 14)) # change font
 url_entry.pack(side=tk.LEFT)
-
-
 
 root.mainloop()
